7b21caf6d687b125bdf0ac054ba6d4844f0e268e
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-10-25 17:37:00 -0600

    created license functions.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,28 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If license returns a license badge, if there is no license, returns an empty string
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![License](https://img.shields.io/badge/License-${license.replace(/ /g, '%20')}-blue.svg)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// If license then returns a license link, if there is no license, returns an empty string
+function renderLicenseLink(license) {
+  if (license) {
+    return `[License Information](https://choosealicense.com/licenses/${license.toLowerCase()})`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// if license, returns license section, if there is no license, returns an empty string
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is licensed under the ${license} License. See the ${renderLicenseLink(license)} for details.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
